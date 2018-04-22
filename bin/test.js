e4cad13acbf9621fd7c19428a8977075c5353481
Gaurav Tiwari <gauravtiwari5050@gmail.com>
2018-04-22 22:09:34 +0530

    add query runner for postgres sql

--- bin/test.js
+++ bin/test.js
@@ -2,12 +2,14 @@
 const { DataSource } = require('../lib/data_sources/data_source');
 
 (async () => {
-  const schema = await DataSource.schema('POSTGRES_SQL', {
+  const schema = await DataSource.query('POSTGRES_SQL', {
     host: 'localhost',
     port: '5342',
     user: 'gauravt',
     password: 'allahabad',
     database: 'sampleapp_development',
-  }, null);
+  }, {
+    query: `SELECT COUNT(*) AS count_all, "events"."category" AS events_category FROM "events" GROUP BY "events"."category"`,
+  });
   console.log(JSON.stringify(schema));
 })();
